926b22a1ebc6d7fc119aca11a91c9c3a64dfb292
hamishhuggard <hamishhuggard@gmail.com>
2024-04-25 13:49:42 +1200

    Basic react component

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <head className="header">
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+            </head>
+        </div>
+    );
+}
+
+export default App;
