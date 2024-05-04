99a53c932c7140e5d05137ab4956f3900c31379d
hamishhuggard <hamishhuggard@gmail.com>
2024-05-04 15:23:25 +1200

    more PIL examples

--- 23-PIL/pil-examples.py
+++ 23-PIL/pil-examples.py
@@ -1,10 +1,16 @@
 from PIL import Image
+from PIL import ImageFilter
 
 with Image.open('./img.png') as img:
     img.save('./new-img.png')
 
     smol_img = img.resize((300,300))
     thumbnailed_img = img.thumbnail((300,300))
+    cropped = img.crop((100, 100, 200, 300))
+    img.rotate(90)
+    img.transpose(Image.FLIP_LEFT_RIGHT)
+    img.filter(ImageFilter.BLUR)
+
     smol_img.save('smol-img.png')
 
 
