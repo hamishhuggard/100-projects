9cd69ad074da77fbb672cc18d144b7956ad64a81
hamishhuggard <hamishhuggard@gmail.com>
2024-06-29 23:30:00 +1200

    typing the todo api again

---
+++ 81-todo-api-again/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express()
+const port = 3000;
+
+let todos = []
+
+app.use(bodyParser.json())
+
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+app.post('/todos', (req, res) => {
+    const { text } = req.body;
+    const todo = { id: todos.length + 1, text };
+    todos.push(todo);
+    res.status(201).json(todo);
+});
+
+app.put('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    const { text } req.body;
+    const index = todos.findIndex(todo => todo.id === parseInt(id));
+    if (index===-1) return res.status(404).send('Todo not found');
+    todos[index].text = text;
+    res.json(todos[index])
+});
+
+app.delete('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    todos = todos.filter(todo => todo.id !== parseInt(id));
+    res.status(204).send();
+});
+
+app.list(port, () => {
+    console.log(`Server running on port ${port}`);
+});
