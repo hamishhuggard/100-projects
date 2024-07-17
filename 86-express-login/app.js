b0d925d96d31e4ec7c83857c69feb16c4f93ecb0
hamishhuggard <hamishhuggard@gmail.com>
2024-07-17 23:59:52 +1200

    start authentication app

--- 86-express-login/app.js
+++ 86-express-login/app.js
@@ -10,3 +10,25 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.listen(3000, () => console.log('server started on port 3000'));
+
+// Local
+
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+
+passport.use(new LocalStrategy({ usernameField: 'email' },
+    async (email, password, done) => {
+        const user = users.find(user => user.email === email);
+        if (!user) return done(null, false, { message: 'No user with that email' });
+
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'Password incorrect' });
+            }
+        } catch (e) {
+            return done(e);
+        }
+    }
+})
