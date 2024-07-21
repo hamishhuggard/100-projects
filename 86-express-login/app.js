1a0e098f47b3fa286dcc7f830dc042dc36912e44
hamishhuggard <hamishhuggard@gmail.com>
2024-07-21 23:57:14 +1200

    start authentication app

--- 86-express-login/app.js
+++ 86-express-login/app.js
@@ -32,3 +32,9 @@ passport.use(new LocalStrategy({ usernameField: 'email' },
         }
     }
 })
+
+passport.serializeUser((User, done) => done(null, user.id));
+passport.deserializeUser((id, done) => {
+    const user = users.find(user => user.id == id);
+    done(null, user);
+})
