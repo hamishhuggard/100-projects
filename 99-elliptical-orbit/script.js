4e1dcd2c695a85ec3c714033e47d6a0e53801e7b
hamishhuggard <hamishhuggard@gmail.com>
2024-08-19 10:23:09 +1200

    broken elliptical orbiter

---
+++ 99-elliptical-orbit/script.js
@@ -0,0 +1,25 @@
+const earth = document.querySelector('.earth');
+const orbit = document.querySelector('.orbit');
+let angle = 0;
+const eccentricity = 0.5; // Adjust the eccentricity here (0 <= eccentricity < 1)
+
+const a = 200; // Semi-major axis
+const b = a * Math.sqrt(1 - eccentricity * eccentricity); // Semi-minor axis
+
+// Set the orbit dimensions based on the calculated a and b
+orbit.style.width = `${2 * a}px`;
+orbit.style.height = `${2 * b}px`;
+orbit.style.marginLeft = `-${a*0.7}px`;
+orbit.style.marginTop = `-${b*0.75}px`;
+
+function rotateEarth() {
+    angle = (angle + 1) % 360;
+    const radians = angle * (Math.PI / 180);
+    const x = a * Math.cos(radians);
+    const y = b * Math.sin(radians);
+
+    earth.style.transform = `translate(${x}px, ${y}px)`;
+    requestAnimationFrame(rotateEarth);
+}
+
+rotateEarth();
