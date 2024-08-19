b26c2a36cfe47fdb867fdf64ee6555e12ec215ad
hamishhuggard <hamishhuggard@gmail.com>
2024-08-19 20:52:46 +1200

    fix eccentricity offsets

--- 99-elliptical-orbit/script.js
+++ 99-elliptical-orbit/script.js
@@ -7,13 +7,19 @@ let angle = 0;
 const a = 200; // Semi-major axis
 let eccentricity = parseFloat(eccentricitySlider.value);
 
-let b;
+let b, c;
 
 function updateOrbit() {
     b = a * Math.sqrt(1 - eccentricity * eccentricity); // Semi-minor axis
+    c = a * eccentricity; // Distance from the center to the focus
+
     orbit.style.width = `${2 * a}px`;
     orbit.style.height = `${2 * b}px`;
 
+    // Adjust the position of the orbit to keep the Sun at the focus
+    //orbit.style.left = `calc(50% - ${a}px - ${c}px)`;
+    earth.style.setProperty('--yOffset', `${-0}px`);
+    orbit.style.setProperty('--yOffset', `${-c}px`);
 }
 
 // Event listener to update eccentricity based on slider value
@@ -32,11 +38,11 @@ function rotateEarth() {
     const x = a * Math.cos(radians);
     const y = b * Math.sin(radians);
 
-    earth.style.setProperty('--dx', `${x}px`);
+    // Move the Earth, taking into account the offset from the center
+    earth.style.setProperty('--dx', `${x - c}px`);
     earth.style.setProperty('--dy', `${y}px`);
 
     requestAnimationFrame(rotateEarth);
 }
 
 rotateEarth();
-
