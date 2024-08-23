72485ca77f4d12fa8f8a946f4838eb1e19609fb9
hamishhuggard <hamishhuggard@gmail.com>
2024-08-24 01:31:53 +1200

    restyling

--- 106-blog/script.js
+++ 106-blog/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     const postsPerPage = 5;
     let posts = [];
+    let currentPage = 1;
+    let totalPages = 0;
 
     function loadPosts(page) {
         const start = (page - 1) * postsPerPage;
@@ -13,28 +15,50 @@ $(document).ready(function() {
                 $('#posts-container').append(`<div class="post">${data}</div>`);
             });
         });
+        updatePagination();
     }
 
-    function createPagination(totalPages) {
-        $('#pagination').empty();
-        for (let i = 1; i <= totalPages; i++) {
-            $('#pagination').append(`<div class="page-button" data-page="${i}">${i}</div>`);
-        }
-        $('.page-button').click(function() {
-            const page = $(this).data('page');
-            $('.page-button').removeClass('active');
-            $(this).addClass('active');
-            loadPosts(page);
-        });
+    function updatePagination() {
+        $('#page-info').text(`Page ${currentPage} / ${totalPages}`);
+        $('#first-page').prop('disabled', currentPage === 1);
+        $('#prev-page').prop('disabled', currentPage === 1);
+        $('#next-page').prop('disabled', currentPage === totalPages);
+        $('#last-page').prop('disabled', currentPage === totalPages);
     }
 
     function init() {
         $.get('posts/index.txt', function(data) {
             posts = data.trim().split('\n').filter(post => post !== 'index.txt');
-            const totalPages = Math.ceil(posts.length / postsPerPage);
-            createPagination(totalPages);
-            loadPosts(1);
-            $('.page-button').first().addClass('active');
+            totalPages = Math.ceil(posts.length / postsPerPage);
+            loadPosts(currentPage);
+        });
+
+        $('#first-page').click(function() {
+            if (currentPage > 1) {
+                currentPage = 1;
+                loadPosts(currentPage);
+            }
+        });
+
+        $('#prev-page').click(function() {
+            if (currentPage > 1) {
+                currentPage--;
+                loadPosts(currentPage);
+            }
+        });
+
+        $('#next-page').click(function() {
+            if (currentPage < totalPages) {
+                currentPage++;
+                loadPosts(currentPage);
+            }
+        });
+
+        $('#last-page').click(function() {
+            if (currentPage < totalPages) {
+                currentPage = totalPages;
+                loadPosts(currentPage);
+            }
         });
     }
 
