22e49f957934a64b4bf15988bab56b99a73be22c
hamishhuggard <hamishhuggard@gmail.com>
2024-09-02 22:32:28 +1200

    pdf generator

--- projects/109-webpage-to-pdf/generate-pdf.js
+++ projects/109-webpage-to-pdf/generate-pdf.js
@@ -3,7 +3,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('file:///path/to/your/cv.html', {waitUntil: 'networkidle0'});
+  //await page.goto('file:///path/to/your/cv.html', {waitUntil: 'networkidle0'});
+  await page.goto('../083-cv/index.html', {waitUntil: 'networkidle0'});
   await page.pdf({
     path: 'cv.pdf',
     format: 'A4',
