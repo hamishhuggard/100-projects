e17c628a49be97b9f5a8e8795a85ea2bd851b648
hamishhuggard <hamishhuggard@gmail.com>
2024-12-03 11:36:48 +1300

    another todo list

---
+++ projects/128-flask-todo/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    complete = db.Column(db.Boolean, default=False)
+    task = db.Column(db.String(200))
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    todos = Todo.query.all()
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=["POST"])
+def add():
+    new_todo = Todo(**request.form)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = Todo.query.get(todo_id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/complete/<int:todo_id>')
+def complete(todo_id):
+    todo = Todo.query.get(todo_id)
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
