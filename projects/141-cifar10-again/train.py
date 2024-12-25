1ed4f19664ead58b42f71c8f288a576343f73f12
hamishhuggard <hamishhuggard@gmail.com>
2024-12-25 23:42:48 +1300

    writing the training mnist code again

---
+++ projects/141-cifar10-again/train.py
@@ -0,0 +1,11 @@
+import torch
+from torch import nn, optim
+import torchvision
+from torchvision import tranforms
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
