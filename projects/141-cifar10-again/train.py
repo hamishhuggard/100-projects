e829eb0ff37adb2493d0cc95c5a488ffca3a6f61
hamishhuggard <hamishhuggard@gmail.com>
2024-12-26 23:56:56 +1300

    continuing to write trian.py

--- projects/141-cifar10-again/train.py
+++ projects/141-cifar10-again/train.py
@@ -9,3 +9,19 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+class SimpleNNN(nn.Module):
+    def __init__(self):
+        self.conv1 = nn.Conv1D(3, 32, kernel_size=2, padding=1)
+
+
+
