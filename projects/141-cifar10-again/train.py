f33702e76dd64d19d8d611a35c92e08ab4c79343
hamishhuggard <hamishhuggard@gmail.com>
2025-01-06 23:23:11 +1300

    More train

--- projects/141-cifar10-again/train.py
+++ projects/141-cifar10-again/train.py
@@ -41,8 +41,17 @@ optimizer = optim.Adam(mode.parameters(), lr=0.001)
 def train(model, trainloader, criterion, optimizer, epochs=10):
     for epoch in epochs:
         running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            inputs, labels = data
-            inputs, labels = inputs.to(device), labels.to(device)
+        for i, [inputs, labels] in enumerate(trainloader, 0):
+            loss = criterion(
+                model(inputs.to(device)),
+                labels.to(device)
+            )
+            loss.backwards()
+            optimizer.step()
+            running_loss += loss.item()
+            if i%100==99:
+                print(f'[{epoch+1}, {i+1:5d}] loss: {running_loss/100:.3f}')
+                running_loss = 0
+        print('finished training')
 
 
