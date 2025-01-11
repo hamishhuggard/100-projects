a2de514539448541b4206f089e630fba991fed60
hamishhuggard <hamishhuggard@gmail.com>
2025-01-11 22:32:19 +1300

    more cifar10

--- projects/141-cifar10-again/train2.py
+++ projects/141-cifar10-again/train2.py
@@ -1,3 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+transform = transforms.Compose([
+    transforms.to_tensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', test=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
@@ -13,19 +34,8 @@ class SimpleCNN(nn.Module):
         x = torch.relu(self.fc1(x))
         return self.fc2(x)
 
-class SimpleCNN(nn.Module):
-    def __init__(self):
-        super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.fc1 = nn.Linear(8*8*64, 512)
-        self.fc2 = nn.Linear(512, 10)
+model = SimpleCNN().to(device)
+criterion = nn.CrossEntropyLoss()
+optim = optim.Adam(model.parameters(), lr=0.001)
 
-    def forward(self, x):
-        x = self.pool(self.conv1(x))
-        x = self.pool(self.conv2(x))
-        x = x.view(-1, 64*8*8)
-        x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+
