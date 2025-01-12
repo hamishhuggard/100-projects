2663cb3bb7acd84276f2e6ba07fdd70b8a9f690a
hamishhuggard <hamishhuggard@gmail.com>
2025-01-13 00:00:45 +1300

    Finish train again

--- projects/141-cifar10-again/train2.py
+++ projects/141-cifar10-again/train2.py
@@ -38,4 +38,35 @@ model = SimpleCNN().to(device)
 criterion = nn.CrossEntropyLoss()
 optim = optim.Adam(model.parameters(), lr=0.001)
 
+def train(model, trainloader, criterion, optimizer, epochs=10):
+    for epoch in range(epochs):
+        running_loss = 0
+        for i, data in enumerate(trainloader, 0):
+            optimizer.zero_grad()
+            loss = criterion(
+                model(input.to(device), labels.to(device)),
+                labels
+            )
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            if i % 100 == 99:
+                print(f'[{}]')
+                running_loss = 0
+    print('Finished training')
 
+def evaluate(model, testloader):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            labels = labels.to(device)
+            _, predicted(
+                model(images.to(device), labels)
+            )
+            total += labels.size(0)
+            correct += (labels == predicted).sum().item()
+    print('')
+
+train(model, trainloader, criterion, optim)
+evaluate(model, testloader)
