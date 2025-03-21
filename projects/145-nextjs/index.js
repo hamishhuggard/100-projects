83f17279521ee6fc84bf0da5ce5c0f7ac7be12cc
hamishhuggard <hamishhuggard@gmail.com>
2025-03-21 23:46:32 +1300

    basic nextjs project

---
+++ projects/145-nextjs/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Example</title>
+            </Head>
+            <main>
+                <p>this is an example</p>
+            </main>
+        </>
+    )
+}
