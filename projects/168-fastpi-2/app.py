60503b789b93f55931ad130e916be67fab2d5943
hamishhuggard <hamishhuggard@gmail.com>
2025-07-10 23:59:17 +1200

    Add some more to the fastAPI 2

--- projects/168-fastpi-2/app.py
+++ projects/168-fastpi-2/app.py
@@ -1,3 +1,14 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import sqlite3
 import os
+
+app = FastAPI()
+
+DB_FILE = "key_value_store.db"
+
+class KeyValuePair(BaseModel):
+    key: str
+    value: str
+
+
