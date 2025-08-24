dac50b892d56883bdc67ebb780137a81d0b95024
hamishhuggard <hamishhuggard@gmail.com>
2025-08-25 09:11:56 +1200

    Minor corrections

--- projects/102-sheeter/script.js
+++ projects/102-sheeter/script.js
@@ -6,27 +6,20 @@ async function fetchCSV(url) {
 }
 
 async function loadTweets() {
-    const usersUrl  = 'https://docs.google.com/spreadsheets/d/1vEOyjh8bhBfDLe-6BHCQxG3B7NqAmqICtZXA4Cewovc/export?format=csv&gid=0&single=true&output=csv;' // Replace with the actual URL
     const tweetsUrl = 'https://docs.google.com/spreadsheets/d/1vEOyjh8bhBfDLe-6BHCQxG3B7NqAmqICtZXA4Cewovc/export?format=csv&gid=1029905748&single=true&output=csv;'
     
-    const [rawTweets, rawUsers] = await Promise.all([fetchCSV(tweetsUrl), fetchCSV(usersUrl)]);
-    console.log({rawTweets, rawUsers})
+    const rawTweets = await fetchCSV(tweetsUrl);
     
     const tweets = rawTweets.slice(1); // Skip header row
-    const users = rawUsers.slice(1);   // Skip header row
     
-    const userMap = {};
-    users.forEach(([handle, username]) => userMap[handle] = username);
-
     const tweetsContainer = document.getElementById('tweets');
-    tweets.forEach(([handle, tweet, datetime, slug]) => {
-        console.log({handle, tweet, datetime, slug})
+    tweets.forEach(([timestamp, username, tweetContent]) => {
         const tweetElement = document.createElement('div');
         tweetElement.className = 'tweet';
         tweetElement.innerHTML = `
-            <div>@${handle}<span class="username"> ${userMap[handle]}</span></div>
-            <div>${tweet}</div>
-            <div class="datetime">${datetime}</div>
+            <div class="username">@${username}</div>
+            <div class="tweet-content">${tweetContent}</div>
+            <div class="datetime">${timestamp}</div>
         `;
         tweetsContainer.appendChild(tweetElement);
     });
